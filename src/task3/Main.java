0953fd1e1c359353635bf9445284b1a7b94fb940
RustamAleev2023 <ridermix@yandex.ru>
2024-01-18 21:36:14 +0200

    Add Temple fixed

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -1,21 +1,27 @@
 package task3;
 
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        TempleClass<Integer> templeClass1 = new TempleClass<Integer>(10, 100);
+        TempleClass<Integer> templeClass1 = new TempleClass<>(10, 100);
         templeClass1.compare(templeClass1.o1, templeClass1.o2);
         templeClass1.add(templeClass1.o1, templeClass1.o2);
+        System.out.println("=====================================");
 
         TempleClass<String> templeClass2 = new TempleClass<>("Hello", "World");
         templeClass2.add(templeClass2.o1, templeClass2.o2);
         templeClass2.compare(templeClass2.o1, templeClass2.o2);
+        System.out.println("=====================================");
+
+        TempleClass<Double> templeClass3 = new TempleClass<>(1.0, 2.2);
+        templeClass3.compare(templeClass3.o1, templeClass3.o2);
+        templeClass3.add(templeClass3.o1, templeClass3.o2);
+        System.out.println("=====================================");
 
 
     }
 
-    static class TempleClass<T extends Object> {
+    static class TempleClass<T> {
         private T o1;
         private T o2;
 
@@ -24,6 +30,7 @@ public class Main {
             this.o1 = o1;
             this.o2 = o2;
         }
+
 
         public void compare(T o1, T o2) {
             if (o1 == o2) {
@@ -39,7 +46,7 @@ public class Main {
             } else if (o1.getClass() == String.class) {
                 System.out.println(o1 + " " + o2);
             } else {
-                System.out.println(" Такие объекты нельзя сложить");
+                System.out.println("Такие объекты нельзя сложить");
             }
         }
 
